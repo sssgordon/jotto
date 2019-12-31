3626d2af76bf2e0cfc15687f78b07307a06ae9f5
sssgordon <gso7@uwo.ca>
2019-12-31 21:26:25 +0100

    Add Congrats component and test utils

--- src/components/Congrats/Congrats.js
+++ src/components/Congrats/Congrats.js
@@ -3,9 +3,20 @@ import React from "react";
 /**
  * Functional react component for congratulatory message.
  * @function
+ * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if 'success' prop is not passed)
  */
 
-export default () => {
-    return <div></div>;
+export default props => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        );
+    } else {
+        return <div data-test="component-congrats"></div>;
+    }
 };
--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -17,8 +17,20 @@ const setup = (props = {}) => {
     return shallow(<Congrats {...props} />);
 };
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, "component-congrats");
+    expect(component.length).toBe(1);
+});
 
-test("renders no text when `success` prop is false", () => {});
+test("renders no text when `success` prop is false", () => {
+    const wrapper = setup({ success: false });
+    const component = findByTestAttr(wrapper, "component-congrats");
+    expect(component.text()).toBe("");
+});
 
-test("rederes non-empty congrats message when 'success' prop is true", () => {});
+test("rederes non-empty congrats message when 'success' prop is true", () => {
+    const wrapper = setup({ success: true });
+    const message = findByTestAttr(wrapper, "congrats-message");
+    expect(message.text().length).not.toBe(0);
+});
