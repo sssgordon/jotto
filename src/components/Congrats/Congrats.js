25217de5272afc163c62e518fa1f29b003e411c0
sssgordon <gso7@uwo.ca>
2019-12-31 18:48:40 +0100

    Setting up congrats test

---
+++ src/components/Congrats/Congrats.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop is not passed)
+ */
+
+export default () => {
+    return <div></div>;
+};
---
+++ src/components/Congrats/Congrats.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import { findByTestAttr } from "../../../test/testUtils";
+
+import Congrats from "./Congrats";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component.
+ * @function setup
+ * @param {object} props - Component props sepcific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+    return shallow(<Congrats {...props} />);
+};
+
+test("renders without error", () => {});
+
+test("renders no text when `success` prop is false", () => {});
+
+test("rederes non-empty congrats message when 'success' prop is true", () => {});
