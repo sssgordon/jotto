f91a125ddf67912e65dc35e1d946deca81d9ccbb
sssgordon <gso7@uwo.ca>
2020-01-02 13:24:13 +0100

    Write test for Input component when success=true

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -45,9 +45,23 @@ describe("render", () => {
     });
 
     describe("word has been guessed", () => {
-        test("renders component without error", () => {});
-        test("does not renders input box", () => {});
-        test("does not renders submit button", () => {});
+        let wrapper;
+        beforeEach(() => {
+            const initialState = { success: true };
+            wrapper = setup(initialState);
+        });
+        test("renders component without error", () => {
+            const component = findByTestAttr(wrapper, "component-input");
+            expect(component.length).toBe(1);
+        });
+        test("does not renders input box", () => {
+            const inputBox = findByTestAttr(wrapper, "input-box");
+            expect(inputBox.length).toBe(0);
+        });
+        test("does not renders submit button", () => {
+            const submitButton = findByTestAttr(wrapper, "submit-button");
+            expect(submitButton.length).toBe(0);
+        });
     });
 });
 
