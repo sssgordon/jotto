e6cf04406778234fd0f1006f74c28f8213cc2bc9
sssgordon <gso7@uwo.ca>
2020-01-03 19:56:15 +0100

    Change axios get url to heroku

--- src/actions/index.js
+++ src/actions/index.js
@@ -31,11 +31,13 @@ export const guessWord = guessedWord => (dispatch, getState) => {
 };
 
 export const getSecretWord = () => dispatch => {
-    return axios.get("http://localhost:3030").then(response =>
-        dispatch({
-            type: actionTypes.SET_SECRET_WORD,
-            // response.data is hwere axios puts data
-            payload: response.data
-        })
-    );
+    return axios
+        .get("http://frozen-atoll-66349.herokuapp.com/")
+        .then(response =>
+            dispatch({
+                type: actionTypes.SET_SECRET_WORD,
+                // response.data is hwere axios puts data
+                payload: response.data
+            })
+        );
 };
